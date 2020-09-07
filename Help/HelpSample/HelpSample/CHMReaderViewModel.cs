c081ad7aabf2f14c4e2154101b686e9d99228171
Mary George <Mary.George@neudesic.com>
2020-09-07 11:09:47 +0530

    initial commit

---
+++ Help/HelpSample/HelpSample/CHMReaderViewModel.cs
@@ -0,0 +1,82 @@
+﻿using HtmlHelp;
+using HtmlHelp.ChmDecoding;
+using HtmlHelp.UIComponents;
+using Microsoft.Win32;
+using System.Data;
+
+namespace CHMReader
+{
+    public class CHMReaderViewModel
+    {
+        private readonly string LM_Key = @"Software\Sample\Help";
+        readonly HtmlHelpSystem _reader = null;
+        DumpingInfo _dmpInfo = null;
+        readonly InfoTypeCategoryFilter _filter = new InfoTypeCategoryFilter();
+
+        string _prefDumpOutput;
+
+        DumpCompression _prefDumpCompression = DumpCompression.Medium;
+
+        DumpingFlags _prefDumpFlags = DumpingFlags.DumpBinaryTOC | DumpingFlags.DumpTextTOC |
+            DumpingFlags.DumpTextIndex | DumpingFlags.DumpBinaryIndex |
+            DumpingFlags.DumpUrlStr | DumpingFlags.DumpStrings;
+
+        string _prefURLPrefix = "mk:@MSITStore:";
+        bool _prefUseHH2TreePics = false;
+
+        public CHMReaderViewModel(string fileName)
+        {
+            _reader = new HtmlHelpSystem();
+            HtmlHelpSystem.UrlPrefix = "mk:@MSITStore:";
+
+            // use temporary folder for data dumping
+            string sTemp = System.Environment.GetEnvironmentVariable("TEMP");
+            if (sTemp.Length <= 0)
+                sTemp = System.Environment.GetEnvironmentVariable("TMP");
+
+            _prefDumpOutput = sTemp;
+
+            // create a dump info instance used for dumping data
+            _dmpInfo =
+                new DumpingInfo(DumpingFlags.DumpBinaryTOC | DumpingFlags.DumpTextTOC |
+                DumpingFlags.DumpTextIndex | DumpingFlags.DumpBinaryIndex |
+                DumpingFlags.DumpUrlStr | DumpingFlags.DumpStrings,
+                sTemp, DumpCompression.Medium);
+
+            LoadRegistryPreferences();
+
+            HtmlHelpSystem.UrlPrefix = _prefURLPrefix;
+            HtmlHelpSystem.UseHH2TreePics = _prefUseHH2TreePics;
+            OpenFile(fileName);
+        }
+
+        private void LoadRegistryPreferences()
+        {
+            RegistryKey regKey = Registry.LocalMachine.CreateSubKey(LM_Key);
+
+            bool bEnable = bool.Parse(regKey.GetValue("EnableDumping", true).ToString());
+
+            _prefDumpOutput = (string)regKey.GetValue("DumpOutputDir", _prefDumpOutput);
+            _prefDumpCompression = (DumpCompression)((int)regKey.GetValue("CompressionLevel", _prefDumpCompression));
+            _prefDumpFlags = (DumpingFlags)((int)regKey.GetValue("DumpingFlags", _prefDumpFlags));
+
+            if (bEnable)
+                _dmpInfo = new DumpingInfo(_prefDumpFlags, _prefDumpOutput, _prefDumpCompression);
+            else
+                _dmpInfo = null;
+
+            _prefURLPrefix = (string)regKey.GetValue("ITSUrlPrefix", _prefURLPrefix);
+            _prefUseHH2TreePics = bool.Parse(regKey.GetValue("UseHH2TreePics", _prefUseHH2TreePics).ToString());
+        }
+
+        private void OpenFile(string fileName)
+        {
+            _reader.OpenFile(fileName, _dmpInfo);
+        }
+
+        public DataTable Search(string keyword)
+        {
+            return _reader.PerformSearch(keyword, 500, true, true);
+        }
+    }
+}
